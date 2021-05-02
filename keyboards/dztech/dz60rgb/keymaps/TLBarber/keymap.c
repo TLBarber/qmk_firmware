e4c2ce4ef548d506c62d98cf3e005853a31e95ec
TLBarber <thomas.l.barber.jr@gmail.com>
2021-05-02 17:41:25 -0400

    updates

--- keyboards/dztech/dz60rgb/keymaps/TLBarber/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/TLBarber/keymap.c
@@ -2,7 +2,7 @@
 --Layer 0--
   ____________________________________________________________________________________________________________________
  |[ESC~]             [1!]-[2@]-[3#]-[4$]-[5%]-[6^]-[7&]-[8*]-[9(]-[0)]-[-_]-[+=]                       [  BackSpace  ]|
- |[ Tab/OSL(1) ]        [Q]-[W]-[E]-[R]-[T]-[Y]-[U]-[I]-[O]-[P]-[[{]-[]}]                                   [   |\   ]|
+ |[ Tab/OSL(3) ]        [Q]-[W]-[E]-[R]-[T]-[Y]-[U]-[I]-[O]-[P]-[[{]-[]}]                                   [   |\   ]|
  |[ctrl]                 [A]-[S]-[D]-[F]-[G]-[H]-[J]-[K]-[L]-[;/:]-['/"]                                  [   Enter  ]|
  |[ Shift ]               [Z]-[X]-[C]-[V]-[B]-[N]-[M]-[</,]-[>/.]-[?//OSL(RSHFT)]                      [↑][del/OSL(2)]|
  |[ctrl][win][alt]        [____________________SPACE__________________][prtscr/OSL(alt)][win/OSL(1)][←][↓][   →   ]   |
@@ -30,7 +30,6 @@
  |[   ▼   ]                  [▼]-[▼]-[▼]-[▼]-[▼]-[▼]-[▼]-[RGBSpd-]-[RGBSpd+]-[▼]    [Brt+][   ▼   ]|
  |[  ▼  ][ ▼ ][▼]             [_________________▼__________________] [▼][▼][RGBMod-][Brt-][RGBMod+]|
  |_________________________________________________________________________________________________|
-
 */
 
 #include QMK_KEYBOARD_H
@@ -38,10 +37,10 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_GESC,        KC_1,    KC_2,            KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,         KC_EQL,  KC_BSPC,
-        LT(1, KC_TAB),         KC_Q,    KC_W,            KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,         KC_RBRC, KC_BSLS,
-        KC_LCTL, KC_A,    KC_S,            KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                  KC_ENT,
+        LT(2, KC_TAB),         KC_Q,    KC_W,            KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,         KC_RBRC, KC_BSLS,
+        LT(3, KC_MEDIA_PLAY_PAUSE), KC_A,    KC_S,            KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                  KC_ENT,
         KC_LSFT,                 KC_Z,            KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_UP,   LT(2, KC_DEL),
-        KC_LCTL,        KC_LGUI, KC_LALT,                            KC_SPC,                    RALT_T(KC_RGUI), LT(1, KC_PSCREEN),   KC_LEFT,         KC_DOWN, KC_RGHT
+        KC_LCTL,        KC_LGUI, KC_LALT,                      KC_SPC,                    RALT_T(KC_RGUI), LT(1, KC_PSCREEN),   KC_LEFT,         KC_DOWN, KC_RGHT
     ),
     [1] = LAYOUT(
         KC_GRAVE,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
@@ -56,5 +55,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EEP_RST,
         _______,          _______, _______, _______, _______, _______, _______, _______, RGB_SPD, RGB_SPI, _______, RGB_VAI, _______,
         _______, _______, _______,                            _______,                   _______, _______, RGB_RMOD, RGB_VAD, RGB_MOD
+    ),
+    [3] = LAYOUT(
+        _______, _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,  _______,  _______,  EEPROM_RESET,
+        _______, KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2, _______, _______, _______, KC_MS_WH_UP, KC_MS_UP, KC_MS_WH_DOWN, _______, _______, _______,  RESET,
+        _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, _______, _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, _______, _______,       KC_MS_BTN1,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2,
+        _______, _______, _______,                            KC_MS_BTN2,                   _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT
     )
 };
