7e03f46c0841a15a6d2e74f6fb267ff7234460c6
TLBarber <thomas.l.barber.jr@gmail.com>
2021-05-13 12:17:33 -0400

    new layout for idobo that I don't have yet and updates for dz60rgbv2

--- keyboards/dztech/dz60rgb/keymaps/TLBarber/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/TLBarber/keymap.c
@@ -47,6 +47,7 @@
 
 enum{
     TEST_MACRO = SAFE_RANGE,
+    SIGNATURE
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record){
@@ -54,6 +55,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record){
         switch(keycode){
             case TEST_MACRO:
             SEND_STRING("This is a test");
+            return false;
+            break;
+            case SIGNATURE:
+            SEND_STRING("Thomas L Barber JR");
             return false;
             break;
         }
@@ -71,23 +76,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [1] = LAYOUT(
         KC_GESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_INS,  KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, RESET,
-        KC_CAPS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_PGUP,          EEP_RST,
-        KC_TRANS,          KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_HOME, KC_END,  KC_PGDN, KC_VOLU, KC_MUTE,
-        KC_TRANS, KC_TRANS, KC_TRANS,                 KC_MEDIA_PLAY_PAUSE,                   KC_TRANS, KC_TRANS, KC_MPRV, KC_VOLD, KC_MNXT
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,
+        KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP,          EEP_RST,
+        KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_END,  KC_PGDN, KC_VOLU, KC_MUTE,
+        KC_TRNS, KC_TRNS, KC_TRNS,                 KC_MEDIA_PLAY_PAUSE,                  TO(4), KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT
     ),
     [2] = LAYOUT(
         RGB_TOG, KC_F13,   KC_F14,   KC_F15,   KC_F16,   KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,   KC_F22,  RGB_SAD,  RGB_SAI,  EEPROM_RESET,
-        KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, RGB_HUD, RGB_HUI, RESET,
-        KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,          EEP_RST,
-        KC_TRANS,          KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, RGB_SPD, RGB_SPI, KC_TRANS, RGB_VAI, KC_TRANS,
-        KC_TRANS, KC_TRANS, KC_TRANS,                            KC_TRANS,                   KC_TRANS, KC_TRANS, RGB_RMOD, RGB_VAD, RGB_MOD
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_HUD, RGB_HUI, RESET,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          EEP_RST,
+        KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_SPD, RGB_SPI, KC_TRNS, RGB_VAI, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                   KC_TRNS, KC_TRNS, RGB_RMOD, RGB_VAD, RGB_MOD
     ),
     [3] = LAYOUT(
-        KC_TRANS, KC_TRANS,   KC_TRANS,   KC_TRANS,   KC_TRANS,   KC_TRANS,   KC_TRANS,   KC_TRANS,   KC_TRANS,   KC_TRANS,   KC_TRANS,  KC_TRANS,  KC_TRANS,  KC_TRANS,
-        KC_TRANS, KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,  KC_TRANS,
-        KC_TRANS, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,       KC_TRANS,
-        KC_TRANS,          KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS, KC_MS_WH_UP, KC_TRANS,
-        KC_TRANS, KC_TRANS, KC_TRANS,                            TEST_MACRO,                   KC_TRANS, KC_TRANS, KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_RIGHT
+        KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+        KC_TRNS, KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+        KC_TRNS, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,
+        KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_WH_UP, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,                            SIGNATURE,                   KC_TRNS, KC_TRNS, KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_RIGHT
+    ),
+    [4] = LAYOUT(
+        KC_GRAVE,                       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_MINS,         KC_EQL,  KC_BSPC,
+        LT(2, KC_TAB),                  KC_W,    KC_L,    KC_R,    KC_B,    KC_Z,    KC_SCLN, KC_Q,    KC_U,    KC_D,    KC_J,          KC_LBRC,         KC_RBRC, KC_BSLS,
+        LT(3, KC_MEDIA_PLAY_PAUSE),     KC_S,    KC_H,    KC_N,    KC_T,    KC_COMM, KC_DOT,  KC_A,    KC_E,    KC_O,    KC_I,          KC_QUOT,                  KC_ENT,
+        KC_LSFT,                        KC_F,    KC_M,    KC_V,    KC_C,    KC_SLSH, KC_G,    KC_P,    KC_X,    KC_K,    RSFT_T(KC_Y),  KC_UP,   LT(2, KC_DEL),
+        KC_LCTL,        KC_LGUI, KC_LALT,                      KC_SPC,                    RALT_T(KC_RGUI), TO(1),  KC_LEFT, KC_DOWN, KC_RGHT
     )
 };
